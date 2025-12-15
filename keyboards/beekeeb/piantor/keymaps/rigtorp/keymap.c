c26a6b08a3871c9be5625787fbbb9eb8e1c2bde7
Erik Rigtorp <erik@rigtorp.se>
2025-12-15 23:26:41 +0100

    Add layer locks

--- keyboards/beekeeb/piantor/keymaps/rigtorp/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/rigtorp/keymap.c
@@ -32,37 +32,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_ESC,         KC_SPC,         KC_TAB,                 KC_ENT,         KC_BSPC,        KC_DEL
     ),
     [L_NAV] = LAYOUT_split_3x5_3(
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          QK_LLCK,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,        KC_NO,                  CW_TOGG,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,
         KC_NO,          KC_ALGR,        KC_NO,          KC_NO,          KC_NO,                  KC_INS,         KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,
                                         KC_NO,          KC_NO,          KC_NO,                  KC_ENT,         KC_BSPC,        KC_DEL
     ),
     [L_MOU] = LAYOUT_split_3x5_3(
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          QK_LLCK,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,        KC_NO,                  KC_NO,          MS_LEFT,        MS_DOWN,        MS_UP,          MS_RGHT,
         KC_NO,          KC_ALGR,        KC_NO,          KC_NO,          KC_NO,                  KC_NO,          MS_WHLL,        MS_WHLD,        MS_WHLU,        MS_WHLR,
                                         KC_NO,          KC_NO,          KC_NO,                  MS_BTN2,        MS_BTN1,        MS_BTN3
     ),
     [L_MED] = LAYOUT_split_3x5_3(
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          QK_LLCK,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,        KC_NO,                  KC_NO,          KC_MPRV,        KC_VOLD,        KC_VOLU,        KC_MNXT,
         KC_NO,          KC_ALGR,        KC_NO,          KC_NO,          KC_NO,                  OU_AUTO,        KC_NO,          KC_NO,          KC_NO,           KC_NO,
                                         KC_NO,          KC_NO,          KC_NO,                  KC_MSTP,        KC_MPLY,        KC_MUTE
     ),
     [L_NUM] = LAYOUT_split_3x5_3(
-        KC_LBRC,        KC_7,           KC_8,           KC_9,           KC_RBRC,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_LBRC,        KC_7,           KC_8,           KC_9,           KC_RBRC,                QK_LLCK,        KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_SCLN,        KC_4,           KC_5,           KC_6,           KC_EQL,                 KC_NO,          KC_LSFT,        KC_LCTL,        KC_LALT,        KC_LGUI,
         KC_GRV,         KC_1,           KC_2,           KC_3,           KC_BSLS,                KC_NO,          KC_NO,          KC_NO,          KC_ALGR,        KC_NO,
                                         KC_DOT,         KC_0,           KC_MINS,                KC_NO,          KC_NO,          KC_NO
     ),
     [L_SYM] = LAYOUT_split_3x5_3(
-        KC_LCBR,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RCBR,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_LCBR,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RCBR,                QK_LLCK,        KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_COLN,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_PLUS,                KC_NO,          KC_LSFT,        KC_LCTL,        KC_LALT,        KC_LGUI,
         KC_TILD,        KC_EXLM,        KC_AT,          KC_HASH,        KC_PIPE,                KC_NO,          KC_NO,          KC_NO,          KC_ALGR,        KC_NO,
                                         KC_LPRN,        KC_RPRN,        KC_UNDS,                KC_NO,          KC_NO,          KC_NO
     ),
     [L_FUN] = LAYOUT_split_3x5_3(
-        KC_F12,         KC_F7,          KC_F8,          KC_F9,          KC_PSCR,                KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_F12,         KC_F7,          KC_F8,          KC_F9,          KC_PSCR,                QK_LLCK,        KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_F11,         KC_F4,          KC_F5,          KC_F6,          KC_SCRL,                KC_NO,          KC_LSFT,        KC_LCTL,        KC_LALT,        KC_LGUI,
         KC_F10,         KC_F1,          KC_F2,          KC_F3,          KC_PAUS,                KC_NO,          KC_NO,          KC_NO,          KC_ALGR,        KC_NO,
                                         KC_APP,         KC_SPC,         KC_TAB,                 KC_NO,          KC_NO,          KC_NO
